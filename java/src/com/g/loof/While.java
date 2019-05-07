408c6d9d3eb61f4b3f70170fbef0d3a8776d7c9f
jsbae <baelong21@naver.com>
2019-05-07 16:58:39 +0900

    loof

---
+++ java/src/com/g/loof/While.java
@@ -0,0 +1,13 @@
+package com.g.loof;
+
+public class While {
+
+	public static void main(String[] args) {
+		int i = 0;
+		//i의 값이 10보다 작다면 true, 크다면 false가된다.
+		while (i < 10) {
+			System.out.println("Coding Everybody" + i);
+			i++;
+		}
+	}
+}
