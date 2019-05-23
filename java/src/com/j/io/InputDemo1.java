d3ed87ee4b8e250e595ab8a96e557e017907e7cb
jsbae <baelong21@naver.com>
2019-05-23 18:01:00 +0900

    input

---
+++ java/src/com/j/io/InputDemo1.java
@@ -0,0 +1,10 @@
+package com.j.io;
+
+public class InputDemo1 {
+
+	public static void main(String[] args) {
+		System.out.println(args.length);
+
+	}
+
+}
---
+++ java/src/com/j/io/InputDemo2.java
@@ -0,0 +1,10 @@
+package com.j.io;
+
+public class InputDemo2 {
+
+	public static void main(String[] args) {
+		for (String e : args) {
+			System.out.println(e);
+		}
+	}
+}
